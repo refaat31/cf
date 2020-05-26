47f2bc04478759a9bccdfaffe196137051e1fc83
refaat1731 <refaat.lannister@gmail.com>
2020-05-26 17:11:33 +0600

    1353A added

---
+++ 1353A - Most Unstable Array.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    int n,m;
+    cin>>t;
+    while(t--){
+        cin>>n>>m;
+        if(n==1) cout<<0<<endl;
+        else if(n==2) cout<<m<<endl;
+        else cout<<2*m<<endl;
+    }
+
+    
+
+}
