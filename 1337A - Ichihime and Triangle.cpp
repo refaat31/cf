20e324514584ef9235f444ccdc307970d3b34324
refaat1731 <refaat.lannister@gmail.com>
2020-05-26 17:53:02 +0600

    1337A added

---
+++ 1337A - Ichihime and Triangle.cpp
@@ -0,0 +1,14 @@
+//How I arrived at the solution is unclear to myself XD
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        cout<<b<<" "<<c<<" "<<c<<" \n";
+    }
+}
